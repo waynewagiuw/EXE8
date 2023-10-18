bad37288f191108a2e25b6fdbaca2bbab7bc3308
tesdoang <s22110290@student.unklab.ac.id>
2023-10-18 20:09:38 +0800

    last

--- src/componnent/Header/Generations/index.jsx
+++ src/componnent/Header/Generations/index.jsx
@@ -1,3 +1,8 @@
+import Gen1 from "./Gen1";
+import Gen2 from "./Gen2";
+import Gen3 from "./Gen3";
+import Gen4 from "./Gen4";
+
 const Generations = () => {
     return (
         <>
---
+++ src/componnent/Home/Card_Info/index.jsx
@@ -0,0 +1,14 @@
+import Evolution from "./Evolutions";
+import Type_Effect from "./Type_Effect";
+
+const Card_info = () => {
+    return (
+        <>
+          <Evolution />
+          <Type_Effect />
+        </>
+      );
+  };
+  
+  export default Card_info;
+  
