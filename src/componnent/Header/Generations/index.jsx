969eacc0794ee34a8e759bb1cfbc0c627b06d5ff
beverlyivy <s22110252@student.unklab.ac.id>
2023-11-05 21:12:21 +0800

    adding gene, logo

--- src/componnent/Header/Generations/index.jsx
+++ src/componnent/Header/Generations/index.jsx
@@ -1,18 +1,5 @@
-import Gen1 from "./Gen1";
-import Gen2 from "./Gen2";
-import Gen3 from "./Gen3";
-import Gen4 from "./Gen4";
+const Generations = ({title}) => {
+  return <p>{title}</p>;
+};
 
-const Generations = () => {
-    return (
-        <>
-          <Gen1 />
-          <Gen2 />
-          <Gen3/>
-          <Gen4 />
-        </>
-      );
-  };
-  
-  export default Generations;
-  
+export default Generations;
